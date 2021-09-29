85bd52b9477aa92b2ce0d21058f03c8f3e6b905a
Jo YoHan <whdygks210@naver.com>
2021-09-29 20:46:36 +0900

    Return greetings for multiple people

--- home/greetings/greetings.go
+++ home/greetings/greetings.go
@@ -20,6 +20,25 @@ func Hello(name string) (string, error) { // 2개의 값을 반환할 수 있다
 	return message, nil // nil => 오류 없음을 의미
 }
 
+// Hellos returns a map that associates each of the named people
+// with a greeting message
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string) // map은 해시테이블(Hash table)을 구현한 자료구조, 형식은 map[Key타입]Value타입
+	// Logo through the received slice of names, calling
+	// the Hollo function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with
+		// the name.
+		messages[name] = message;
+	}
+	return messages, nil
+}
+
 
 // init sets initial values for variables used in the function.
 func init()  {
