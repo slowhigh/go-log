fd33a036bd0568a028ffc6271ed2260e0670b47c
Jo YoHan <whdygks210@naver.com>
2021-10-12 17:02:33 +0900

    Developing a RESTful API with Go and Gin - 2

--- RESTful_API_with_Go_and_Gin/web-service-gin/main.go
+++ RESTful_API_with_Go_and_Gin/web-service-gin/main.go
@@ -6,20 +6,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
 // album represents data about a record album.
 // Struct tags such as json:"artist" specify what a field’s name should be when the struct’s contents are serialized into JSON. Without them, the JSON would use the struct’s capitalized field names – a style not as common in JSON.
 type album struct {
-	ID string `json:"id"`
-	Title string `json:"title"`
-	Artist string `json:"artist"`
-	Price float64 `json:"price"`
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
 
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
 func main() {
@@ -27,19 +26,21 @@ func main() {
 	router.GET("/albums", getAlbums)
 	//Note that you’re passing the name of the getAlbums function. This is different from passing the result of the function, which you would do by passing getAlbums() (note the parenthesis).
 
-	router.POST("/albums", postAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
 
-
 // getAlbum responds with the list of all albums as JSON
+// curl http://localhost:8080/albums --header "Content-Type: application/json" --request "GET"
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
 // postAlbums adds an album from JSON received in the request body.
+// curl http://localhost:8080/albums --include --header "Content-Type: application/json" --request "POST" --data "{\"id\": \"4\",\"title\": \"The Modern Sound of Betty Carter\",\"artist\": \"Betty Carter\",\"price\": 49.99}"
 func postAlbums(c *gin.Context) {
 	var newAlbum album
 
@@ -51,6 +52,22 @@ func postAlbums(c *gin.Context) {
 	// Add the new album to the slice/
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 //At the command line, use go get to add the github.com/gin-gonic/gin module as a dependency for your module. Use a dot argument to mean “get dependencies for code in the current directory.”
