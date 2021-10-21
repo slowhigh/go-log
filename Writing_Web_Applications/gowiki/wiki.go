bd4284f226c978b9f523bd940e581e889f794450
Jo YoHan <whdygks210@naver.com>
2021-10-21 20:05:58 +0900

    Writing Web Applications - End
    - Introducing Function Literals and Closures

--- Writing_Web_Applications/gowiki/wiki.go
+++ Writing_Web_Applications/gowiki/wiki.go
@@ -39,24 +39,55 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):] // len("/view/") => 6, [A:B] => index의 A부터 B까지 부분 문자열, ex) r.URL.Path = /view/TestPage, r.URL.Path[len("/view/"):] => TestPage
-	log.Println("title : " + title)
-	//p, _ := loadPage(title) // Again, note the use of _ to ignore the error return value from loadPage. This is done here for simplicity and generally considered bad practice.
+//  func viewHandler(w http.ResponseWriter, r *http.Request) {
+// 	//title := r.URL.Path[len("/view/"):] // len("/view/") => 6, [A:B] => index의 A부터 B까지 부분 문자열, ex) r.URL.Path = /view/TestPage, r.URL.Path[len("/view/"):] => TestPage
+// 	//log.Println("title : " + title)
+// 	title, err := getTitle(w, r)
+// 	if err != nil {
+// 		return
+// 	}
+	
+// 	//p, _ := loadPage(title) // Again, note the use of _ to ignore the error return value from loadPage. This is done here for simplicity and generally considered bad practice.
+// 	p, err := loadPage(title)
+// 	if err != nil {
+// 		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+// 		return
+// 	}
+// 	renderTemplate(w, "view", p)
+// }
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
+
 	if err != nil {
 		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
 		return
 	}
+
 	renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+// func editHandler(w http.ResponseWriter, r *http.Request) {
+// 	// title := r.URL.Path[len("/edit/"):]
+// 	title, err := getTitle(w, r)
+// 	if err != nil {
+// 		return
+// 	}
+	
+// 	p, err := loadPage(title)
+// 	if err != nil {
+// 		p = &Page{Title: title}
+// 	}
+// 	renderTemplate(w, "edit", p)
+// }
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
+
 	if err != nil {
 		p = &Page{Title: title}
 	}
+
 	renderTemplate(w, "edit", p)
 }
 
@@ -78,15 +109,33 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
     }
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+// func saveHandler(w http.ResponseWriter, r *http.Request) {
+// 	// title := r.URL.Path[len("/save/"):]
+// 	title, err := getTitle(w, r)
+// 	if err != nil {
+// 		return
+// 	}
+	
+// 	body := r.FormValue("body")
+// 	p := &Page{Title: title, Body: []byte(body)}
+// 	err = p.save()
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+// 	http.Redirect(w, r, "/view/" + title, http.StatusFound)
+// }
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	
 	http.Redirect(w, r, "/view/" + title, http.StatusFound)
 }
 
@@ -99,15 +148,28 @@ func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
 	return m[2], nil // The title is the second subexpression.
 }
 
+// The returned function is called a closure because it encloses values defined outside of it.
+func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
 func main() {
 	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
 	// p1.save()
 	// p2, _ := loadPage("TestPage")
 	// fmt.Println(string(p2.Body))
 
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
