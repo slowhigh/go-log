6ef498ea165239086f393029411111ec6201d7f9
Jo YoHan <whdygks210@naver.com>
2021-12-30 18:54:23 +0900

    docs : update content

--- Effective_Go/main.go
+++ Effective_Go/main.go
@@ -2,31 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io"
 )
 
-type ByteSlice []byte
+type Sequence []int
 
-func (slice ByteSlice) Append1(data []byte) []byte {
-    return append(slice, data...)
+func (s Sequence) Len() int {
+	return len(s)
 }
 
-func (p *ByteSlice) Append2(data []byte) {
-    slice := *p
-    slice = append(slice, data...)
-    *p = slice
+func (s Sequence) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s Sequence) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func(s Sequence) Copy() Sequence {
+	copy := make(Sequence, 0, len(s))
+	return append(copy, s...)
 }
 
 func main() {
-	var x ByteSlice = []byte{1, 2, 3}
-	var y ByteSlice = []byte{4, 5, 6}
 	
-	fmt.Printf("append = %x \n", x.Append1(y))
-	fmt.Printf("x = %x \n", x)
-	fmt.Printf("y = %x \n", y)
-
-
-    x.Append2(y)
-
-    fmt.Printf("new x = %x", x)
 }
-
