ccb21442411ed38e1f83d527fbfe87946d844a41
Jo YoHan <whdygks210@naver.com>
2022-01-19 15:27:10 +0900

    Update Coding Test : report_result_receive
    Score : 29.2 / 100.0

--- coding_test/programmers/report_result_receive/main.go
+++ coding_test/programmers/report_result_receive/main.go
@@ -6,20 +6,42 @@ import (
 )
 
 func solution(id_list []string, report []string, k int) []int {
+	id_map := make(map[string]string)
+	result_map := make(map[string]int)
+	result_arr := make([]int, len(id_list))
 
-	id_map := make(map[string][]string)
 	for i := 0; i < len(id_list); i++ {
-		id_map[id_list[i]] = make([]string, 0, 4)
+		id_map[id_list[i]] = ""
+		result_map[id_list[i]] = 0
 	}
 
-	for _, item := range report {
-		item_array := strings.Split(item, " ")
+	for i := 0; i < len(report); i++ {
+		report_split_array := strings.Split(report[i], " ")
 
-		strings.Contains(id_map[item_array[]])
-		id_map[item_array[0]] = append(id_map[item_array[0]], item_array[1])
+		if strings.Contains(id_map[report_split_array[1]], report_split_array[0]) {
+			continue
+		}
+
+		id_map[report_split_array[1]] += report_split_array[0] + " "
 	}
 
-	return []int{}
+	for _, value := range id_map {
+		value_split := strings.Split(strings.TrimSpace(value), " ")
+
+		if len(value_split) < k {
+			continue
+		}
+
+		for j := 0; j < len(value_split); j++ {
+			result_map[value_split[j]] += 1
+		}
+	}
+
+	for i, v := range id_list {
+		result_arr[i] = result_map[v]
+	}
+
+	return result_arr
 }
 
 func main() {
