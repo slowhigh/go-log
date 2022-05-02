9c0726091af5139de7764f2326a2947a0a901a5b
Jo YoHan <whdygks210@naver.com>
2022-05-02 18:39:34 +0900

    [update] design-pattern
    - singletone demo (3 examples)

--- design-pattern/singleton/singleton.go
+++ design-pattern/singleton/singleton.go
@@ -1,30 +1,93 @@
 package sigleton
 
-// interface는 해당 모듈의 모든 타입이 기본적으로 구현되야 하는 메소드를 정의 한다.
+import "sync"
+
 type Singleton interface {
-	PlusOne() int
-	MinusOne() int
+	AddCount(value int)
+	GetCount() int
 }
 
-type singleton struct {
+///
+// Example 1 - sync
+///
+type singleton_1 struct {
 	count int
 }
 
-var instance *singleton
+var instance_1 *singleton_1
 
-func GetInstance() Singleton {
-	if instance == nil {
-		instance = new(singleton)
+func GetInstance_1() Singleton {
+
+	if instance_1 == nil {
+		instance_1 = new(singleton_1)
 	}
-	return instance
+
+	return instance_1
 }
 
-func (s *singleton) PlusOne() int {
-	s.count++
+func (s *singleton_1) AddCount(value int) {
+	s.count += value
+}
+
+func (s *singleton_1) GetCount() int {
 	return s.count
 }
 
-func (s *singleton) MinusOne() int {
-	s.count--
+///
+// Example 2 - async
+///
+var lock = &sync.Mutex{}
+
+type singleton_2 struct {
+	count int
+}
+
+var instance_2 *singleton_2
+
+func GetInstance_2() Singleton {
+
+	lock.Lock()
+	defer lock.Unlock()
+
+	if instance_2 == nil {
+		instance_2 = new(singleton_2)
+	}
+
+	return instance_2
+}
+
+func (s *singleton_2) AddCount(value int) {
+	s.count += value
+}
+
+func (s *singleton_2) GetCount() int {
+	return s.count
+}
+
+///
+// Example 3 - async
+///
+
+var once sync.Once
+
+type singleton_3 struct {
+	count int
+}
+
+var instance_3 *singleton_3
+
+func GetInstance_3() Singleton {
+	once.Do(func() {
+		instance_3 = new(singleton_3)
+	})
+
+	return instance_3
+}
+
+func (s *singleton_3) AddCount(value int) {
+	s.count += value
+}
+
+func (s *singleton_3) GetCount() int {
 	return s.count
 }
