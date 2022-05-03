43e73dbf168e179916a1a891d29f55d50ee84699
Jo YoHan <whdygks210@naver.com>
2022-05-03 13:14:38 +0900

    [update] design pattern
    - singleton test

--- design-pattern/singleton/singleton_test.go
+++ design-pattern/singleton/singleton_test.go
@@ -6,15 +6,33 @@ import (
 )
 
 func Test_Singleton(t *testing.T) {
-	a := GetInstance()
-	a.PlusOne()
+	input := []int {1, 1}
+	output := 2
 
-	b := GetInstance()
-	b.PlusOne()
+	a1 := GetInstance_1()
+	a1.AddCount(input[0])
+	a2 := GetInstance_1()
+	a2.AddCount(input[1])
 
-	c := GetInstance()
+	b1 := GetInstance_2()
+	b1.AddCount(input[0])
+	b2 := GetInstance_2()
+	b2.AddCount(input[1])
 
-	if c.MinusOne() != 1 {
-		t.Error(errors.New("s count is not 1"))
+	c1 := GetInstance_3()
+	c1.AddCount(input[0])
+	c2 := GetInstance_3()
+	c2.AddCount(input[1])
+
+	if a2.GetCount() != output {
+		t.Error(errors.New("Wrong output. - Case 1"))
+	}
+
+	if b2.GetCount() != output {
+		t.Error(errors.New("Wrong output. - Case 2"))
+	}
+
+	if c2.GetCount() != output {
+		t.Error(errors.New("Wrong output. - Case 3"))
 	}
 }
