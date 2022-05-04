07d2444ba7fd4356436491864809af883dc07b92
Jo YoHan <whdygks210@naver.com>
2022-05-04 12:31:39 +0900

    [add] design pattern : factory method

---
+++ design-pattern/factory_method/factory_method.go
@@ -0,0 +1,43 @@
+package factory_method
+
+import (
+	"errors"
+	"fmt"
+)
+
+type PaymentMethod interface {
+	Pay(amount float32) string
+}
+
+const (
+	Cash      = 1
+	DebitCard = 2
+)
+
+func GetPaymentMethod(m int) (PaymentMethod, error) {
+	switch m {
+	case Cash:
+		return new(CashPM), nil
+	case DebitCard:
+		return new(DebitCardPM), nil
+	default:
+		return nil, errors.New(fmt.Sprintf("Payment method %d not recognized\n", m))
+	}
+}
+
+type CashPM struct{}
+type DebitCardPM struct{}
+
+func (c *CashPM) Pay(amount float32) string {
+	return fmt.Sprintf("%0.2f paid using cash\n", amount)
+}
+
+func (c *DebitCardPM) Pay(amount float32) string {
+	return fmt.Sprintf("%0.2f paid using debit card\n", amount)
+}
+
+type CreditCardPM struct{}
+
+func (c *CreditCardPM) Pay(amount float32) string {
+	return fmt.Sprintf("%0.2f paid using credit card (new)\n", amount)
+}
