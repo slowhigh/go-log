d82a6a95c74b0ca5d89c0565709f57dd145a3812
Jo YoHan <whdygks210@naver.com>
2022-08-04 18:49:02 +0900

    [feat] mongoDB test

--- go_labs/mongodb/mongodb_test.go
+++ go_labs/mongodb/mongodb_test.go
@@ -12,68 +12,82 @@ import (
 )
 
 type testData struct {
-	ID string `json:"id" bson:"_id"`
+	ID int64  `json:"id" bson:"_id"`
 	A  string `json:"a"`
 	B  string `json:"b"`
 }
+
+var (
+	testDataArr = []testData{
+		{
+			ID: 0,
+			A:  "a_0",
+			B:  "b_0",
+		},
+		{
+			ID: 1,
+			A:  "a_1",
+			B:  "b_1",
+		},
+		{
+			ID: 2,
+			A:  "a_2",
+			B:  "b_2",
+		},
+		{
+			ID: 3,
+			A:  "a_3",
+			B:  "b_3",
+		},
+		{
+			ID: 4,
+			A:  "a_4",
+			B:  "b_4",
+		},
+		{
+			ID: 5,
+			A:  "a_5",
+			B:  "b_5",
+		},
+		{
+			ID: 6,
+			A:  "a_6",
+			B:  "b_6",
+		},
+		{
+			ID: 7,
+			A:  "a_7",
+			B:  "b_7",
+		},
+		{
+			ID: 8,
+			A:  "a_8",
+			B:  "b_8",
+		},
+	}
+)
 
 func Test_CRUD(t *testing.T) {
 	Test_InsertOne(t)
 
 	Test_InsertMany(t)
 
-	Test_Find(t)
-}
+	Test_FindOne(t)
 
-var (
-	testDataArr = []testData{
-		{
-			ID: "0",
-			A:  "a_0",
-			B:  "b_0",
-		},
-		{
-			ID: "1",
-			A:  "a_1",
-			B:  "b_1",
-		},
-		{
-			ID: "2",
-			A:  "a_2",
-			B:  "b_2",
-		},
-		{
-			ID: "3",
-			A:  "a_3",
-			B:  "b_3",
-		},
-		{
-			ID: "4",
-			A:  "a_4",
-			B:  "b_4",
-		},
-		{
-			ID: "5",
-			A:  "a_5",
-			B:  "b_5",
-		},
-		{
-			ID: "6",
-			A:  "a_6",
-			B:  "b_6",
-		},
-		{
-			ID: "7",
-			A:  "a_7",
-			B:  "b_7",
-		},
-		{
-			ID: "8",
-			A:  "a_8",
-			B:  "b_8",
-		},
-	}
-)
+	Test_Find(t)
+
+	Test_ReplaceOne(t)
+
+	Test_UpdateOne(t)
+
+	Test_UpdateByID(t)
+
+	Test_UpdateMany(t)
+
+	Test_DeleteOne(t)
+
+	Test_DeleteMany(t)
+}
 
 func Test_InsertOne(t *testing.T) {
 	coll, close := GetCollection()
@@ -114,7 +128,7 @@ func Test_InsertMany(t *testing.T) {
 	}
 }
 
-func Test_Find(t *testing.T) {
+func Test_FindOne(t *testing.T) {
 	coll, close := GetCollection()
 	defer close()
 
@@ -139,112 +153,151 @@ func Test_Find(t *testing.T) {
 	}
 }
 
-// => 여기까지 수정함
-
-func Test_DeleteOne(t *testing.T) {
+func Test_Find(t *testing.T) {
 	coll, close := GetCollection()
 	defer close()
 
-	filter := bson.M{"title": "title"} // filter := bson.D{{"title","title"}}
+	filter := bson.M{}
 
-	res, err := coll.DeleteOne(context.TODO(), filter)
+	cur, err := coll.Find(context.TODO(), filter)
 	if err != nil {
 		t.Error(err)
 	}
 
-	t.Log(res.DeletedCount)
-}
+	var resultArr []testData
 
-func Test_DeleteMany(t *testing.T) {
-	coll, close := GetCollection()
-	defer close()
-
-	filter := bson.M{"_id": "1"} // filter := bson.D{{"title","title"}}
-
-	res, err := coll.DeleteMany(context.TODO(), filter)
+	err = cur.All(context.TODO(), &resultArr)
 	if err != nil {
 		t.Error(err)
 	}
 
-	t.Log(res.DeletedCount)
-}
-
-func Test_UpdateOne(t *testing.T) {
-	coll, close := GetCollection()
-	defer close()
-
-	data := testData{
-		ID:   "2",
-		A:    "title_updateone",
-		B: "text_updateone",
+	for i := 0; i < len(resultArr); i++ {
+		if resultArr[i].ID != testDataArr[i].ID || resultArr[i].A != testDataArr[i].A || resultArr[i].B != testDataArr[i].B {
+			t.Error("Invalid result")
+		}
 	}
-
-	filter := bson.M{"_id": data.ID}
-	update := bson.M{"$set": bson.M{"title": data.A, "text": data.B}}
-
-	res, err := coll.UpdateOne(context.TODO(), filter, update)
-	if err != nil {
-		t.Error(err)
-	}
-
-	t.Log(res.MatchedCount)
-}
-
-func Test_UpdateByID(t *testing.T) {
-	coll, close := GetCollection()
-	defer close()
-
-	data := testData{
-		ID:   "3",
-		A:    "title_updateone",
-		B: "text_updateone",
-	}
-
-	filter := data.ID
-	update := bson.M{"$set": bson.M{"title": data.A, "text": data.B}}
-
-	res, err := coll.UpdateByID(context.TODO(), filter, update)
-	if err != nil {
-		t.Error(err)
-	}
-
-	t.Log(res.MatchedCount)
-}
-
-func Test_UpdateMany(t *testing.T) {
-	coll, close := GetCollection()
-	defer close()
-
-	filter := bson.M{"text": "text"}
-	update := bson.M{"$set": bson.M{"title": "title"}}
-
-	res, err := coll.UpdateMany(context.TODO(), filter, update)
-	if err != nil {
-		t.Error(err)
-	}
-
-	t.Log(res.MatchedCount)
 }
 
 func Test_ReplaceOne(t *testing.T) {
 	coll, close := GetCollection()
 	defer close()
 
-	data := testData{
-		ID:   "2",
-		A:    "title_updateone",
-		B: "text_updateone",
+	type R_testData struct {
+		ID int64  `json:"id" bson:"_id"`
+		C  string `json:"c"`
 	}
 
-	filter := bson.M{"_id": data.ID}
-	update := bson.M{"title": data.A}
+	targetDataID := testDataArr[0].ID
 
-	res, err := coll.ReplaceOne(context.TODO(), filter, update)
+	filter := bson.M{"_id": targetDataID}
+	replacement := R_testData{
+		ID: 0,
+		C:  "c_0",
+	}
+
+	res, err := coll.ReplaceOne(context.TODO(), filter, replacement)
 	if err != nil {
 		t.Error(err)
 	}
 
-	t.Log(res.MatchedCount)
+	if res.MatchedCount != 1 || res.ModifiedCount != 1 || res.UpsertedCount != 0 || res.UpsertedID != nil {
+		t.Error("Invalid result")
+	}
+}
+
+func Test_UpdateOne(t *testing.T) {
+	coll, close := GetCollection()
+	defer close()
+
+	targetData := testDataArr[1]
+	targetData.A += "_update"
+	targetData.B += "_update"
+
+	filter := bson.M{"a": testDataArr[1].A}
+	update := bson.M{"$set": targetData}
+
+	res, err := coll.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.MatchedCount != 1 || res.ModifiedCount != 1 || res.UpsertedCount != 0 || res.UpsertedID != nil {
+		t.Error("Invalid result")
+	}
+}
+
+func Test_UpdateByID(t *testing.T) {
+	coll, close := GetCollection()
+	defer close()
+
+	targetData := testDataArr[2]
+	targetData.A += "updatebyid"
+	targetData.B += "updatebyid"
+
+	filter := targetData.ID
+	update := bson.M{"$set": targetData}
+
+	res, err := coll.UpdateByID(context.TODO(), filter, update)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.MatchedCount != 1 || res.ModifiedCount != 1 || res.UpsertedCount != 0 || res.UpsertedID != nil {
+		t.Error("Invalid result")
+	}
+}
+
+func Test_UpdateMany(t *testing.T) {
+	coll, close := GetCollection()
+	defer close()
+
+	filter := bson.M{"_id": bson.M{"$gte": testDataArr[3].ID}}
+	update := bson.M{"$set": bson.M{"a": "a", "b": "b"}}
+
+	res, err := coll.UpdateMany(context.TODO(), filter, update)
+	if err != nil {
+		t.Error(err)
+	}
+
+	updatedCount := len(testDataArr[3:])
+
+	if res.MatchedCount != int64(updatedCount) || res.ModifiedCount != int64(updatedCount) || res.UpsertedCount != 0 || res.UpsertedID != nil {
+		t.Error("Invalid result")
+	}
+}
+
+func Test_DeleteOne(t *testing.T) {
+	coll, close := GetCollection()
+	defer close()
+
+	targetData := testDataArr[0]
+
+	filter := bson.M{"_id": targetData.ID}
+
+	res, err := coll.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.DeletedCount != 1 {
+		t.Error("Invalid result")
+	}
+}
+
+func Test_DeleteMany(t *testing.T) {
+	coll, close := GetCollection()
+	defer close()
+
+	filter := bson.M{"_id": bson.M{"$gte": testDataArr[1].ID}}
+
+	res, err := coll.DeleteMany(context.TODO(), filter)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.DeletedCount != int64(len(testDataArr[1:])) {
+		t.Error("Invalid result")
+	}
 }
 
 func GetCollection() (*mongo.Collection, func()) {
@@ -262,3 +315,42 @@ func GetCollection() (*mongo.Collection, func()) {
 
 	return client.Database("testDB").Collection("data"), disconnectDB
 }
+
+/// Comparison Query Operators
+// $eq	: Matches values that are equal to a specified value.
+// $gt	: Matches values that are greater than a specified value.
+// $gte	: Matches values that are greater than or equal to a specified value.
+// $in	: Matches any of the values specified in an array.
+// $lt	: Matches values that are less than a specified value.
+// $lte	: Matches values that are less than or equal to a specified value.
+// $ne	: Matches all values that are not equal to a specified value.
+// $nin	: Matches none of the values specified in an array.
+// ex) filter := bson.D{{"rating", bson.D{{"$lt", 7}}}}
+
+/// Logical Query Operators
+// $and	: Joins query clauses with a logical AND returns all documents that match the conditions of both clauses.
+// $not	: Inverts the effect of a query expression and returns documents that do not match the query expression.
+// $nor	: Joins query clauses with a logical NOR returns all documents that fail to match both clauses.
+// $or	: Joins query clauses with a logical OR returns all documents that match the conditions of either clause.
+// ex)	filter := bson.D{
+//			{"$and",
+//			   bson.A{
+//				  bson.D{{"rating", bson.D{{"$gt", 7}}}},
+//				  bson.D{{"rating", bson.D{{"$lte", 10}}}},
+//			   },
+//			},
+//		}
+
+/// Element Query Operators
+// $exists	: Matches documents that have the specified field.
+// $type	: Selects documents if a field is of the specified type.
+// ex) filter := bson.D{{"vendor", bson.D{{"$exists", false}}}}
+
+/// Evaluation Query Operators
+// $expr		 : Allows use of aggregation expressions within the query language.
+// $jsonSchema	 : Validate documents against the given JSON Schema.
+// $mod			 : Performs a modulo operation on the value of a field and selects documents with a specified result.
+// $regex		 : Selects documents where values match a specified regular expression.
+// $text		 : Performs text search.
+// $where		 : Matches documents that satisfy a JavaScript expression.
+// ex) filter := bson.D{{"type", bson.D{{"$regex", "^E"}}}}
