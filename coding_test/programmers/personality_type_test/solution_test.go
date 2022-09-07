76067fcb00bf5f8cfec1fce77078cd49f7a3cef4
Jo YoHan <whdygks210@naver.com>
2022-09-07 18:19:32 +0900

    [test] Programmers Problem : 성격 유형 검사하기
    - 테스트 코드 추가

--- coding_test/programmers/personality_type_test/solution_test.go
+++ coding_test/programmers/personality_type_test/solution_test.go
@@ -1,1 +1,30 @@
 package personality_type_test
+
+import "testing"
+
+type testCase struct {
+	survey  []string
+	choices []int
+	result  string
+}
+
+func Test_solution(t *testing.T) {
+	testCaseArr := []testCase {
+		{
+			[]string { "AN", "CF", "MJ", "RT", "NA" },
+			[]int { 5, 3, 2, 7, 5 },
+			"TCMA",
+		},
+		{
+			[]string { "TR", "RT", "TR" },
+			[]int { 7, 1, 3 },
+			"RCJA",
+		},
+	}
+
+	for i, tc := range testCaseArr {
+		if solution(tc.survey, tc.choices) != tc.result {
+			t.Errorf("Wrong result - %d", i)
+		}
+	}
+}
