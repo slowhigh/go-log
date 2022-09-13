237238777e795a84eda9c3f88baae979723be4ae
Jo YoHan <whdygks210@naver.com>
2022-09-13 18:48:31 +0900

    add: Programmers Problem - # 약수의 개수와 덧셈

---
+++ coding_test/programmers/count_and_addition_of_divisor/solution.go
@@ -0,0 +1,25 @@
+package count_and_addition_of_divisor
+
+import (
+	"math"
+)
+
+func solution(left int, right int) int {
+	if left == 1 && right == 1 {
+		return -1
+	}
+
+	sum := 0
+
+	for i := left; i <= right; i++ {
+		sqrt := math.Sqrt(float64(i))
+		
+		if sqrt == math.Floor(sqrt) {
+			sum -= i
+		} else {
+			sum += i
+		}
+	}
+
+	return sum
+}
