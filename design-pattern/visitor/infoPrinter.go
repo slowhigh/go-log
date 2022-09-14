25b9fdbac3867c194de1f1381a6676c51d68d0c5
Jo YoHan <whdygks210@naver.com>
2022-09-14 12:51:03 +0900

    fix: design pattern : visitor

--- design-pattern/visitor/infoPrinter.go
+++ design-pattern/visitor/infoPrinter.go
@@ -3,18 +3,18 @@ package visitor
 
 import "fmt"
 
-type infoPrinter struct{
+type infoPrinter struct {
 	print string
 }
 
 func (i *infoPrinter) visitForTriangle(t *triangle) {
-	i.print = fmt.Sprintf() t.width * t.height / 2
+	i.print = fmt.Sprint(t.width * t.height / 2)
 }
 
 func (i *infoPrinter) visitForCircle(c *circle) {
-	i.print = c.radius * c.radius * 3.14
+	i.print = fmt.Sprint(c.radius * c.radius * 3.14)
 }
 
 func (i *infoPrinter) visitForRectangle(r *rectangle) {
-	i.print = r.width * r.height
+	i.print = fmt.Sprint(r.width * r.height)
 }
