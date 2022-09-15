12acc6ad57d6a80fadb0d71e7cd1bb2e9af80442
Jo YoHan <whdygks210@naver.com>
2022-09-15 19:39:38 +0900

    add: Programmers Problem - 피보나치 수

---
+++ coding_test/programmers/fibonacci_number/solution.go
@@ -0,0 +1,14 @@
+package fibonacci_number
+
+func solution(n int) int {
+	a, b := 0, 1
+    var c int
+
+    for i := 1; i < n; i++ {
+        c = (a + b)%1234567
+        a = b
+        b = c
+    }
+
+	return c
+}
