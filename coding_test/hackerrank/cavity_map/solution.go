d92ea9b46b8a045395ab2a99f0a9840cb4cf65bb
Jo YoHan <whdygks210@naver.com>
2023-11-16 19:56:42 +0900

    solving: HackerRank Problem - Cavity Map

---
+++ coding_test/hackerrank/cavity_map/solution.go
@@ -0,0 +1,9 @@
+package cavity_map
+
+// Cavity Map
+// https://www.hackerrank.com/challenges/cavity-map/problem?isFullScreen=false
+
+func cavityMap(grid []string) []string {
+	// Write your code here
+
+}
---
+++ coding_test/hackerrank/cavity_map/solution_test.go
@@ -0,0 +1,1 @@
+package cavity_map
