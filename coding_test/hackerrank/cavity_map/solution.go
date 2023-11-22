e30d87b877dea46c49a346e3970c53b760bb19ff
Jo YoHan <whdygks210@naver.com>
2023-11-22 18:24:11 +0900

    solved: HackerRank Problem - Cavity Map

--- coding_test/hackerrank/cavity_map/solution.go
+++ coding_test/hackerrank/cavity_map/solution.go
@@ -4,6 +4,15 @@ package cavity_map
 // https://www.hackerrank.com/challenges/cavity-map/problem?isFullScreen=false
 
 func cavityMap(grid []string) []string {
-	// Write your code here
+	gridLen := len(grid)
 
+	for i := 1; i < gridLen-1; i++ {
+		for j := 1; j < gridLen-1; j++ {
+			if grid[i][j] > grid[i-1][j] && grid[i][j] > grid[i][j+1] && grid[i][j] > grid[i+1][j] && grid[i][j] > grid[i][j-1] {
+				grid[i] = grid[i][:j] + "X" + grid[i][j+1:]
+			}
+		}
+	}
+
+	return grid
 }
--- coding_test/hackerrank/cavity_map/solution_test.go
+++ coding_test/hackerrank/cavity_map/solution_test.go
@@ -1,1 +1,39 @@
 package cavity_map
+
+import (
+	"fmt"
+	"testing"
+)
+
+type testCase struct {
+	grid   []string
+	result []string
+}
+
+func Test_cavityMap(t *testing.T) {
+	testCaseArr := []testCase{
+		{
+			grid: []string{
+				"1112",
+				"1912",
+				"1892",
+				"1234",
+			},
+			result: []string{
+				"1112",
+				"1X12",
+				"18X2",
+				"1234",
+			},
+		},
+	}
+
+	for i, tc := range testCaseArr {
+		res := fmt.Sprintf("%+v", cavityMap(tc.grid))
+		if res == fmt.Sprintf("%+v", tc.result) {
+			t.Logf("PASS - %d (%s)", i, res)
+		} else {
+			t.Errorf("FAIL - %d (%s)", i, res)
+		}
+	}
+}
