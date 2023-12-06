19ab566a65ccbcbbb4c5eba1dd8fd6b35f2344f8
Jo YoHan <whdygks210@naver.com>
2023-12-06 09:10:56 +0900

    solved: HackerRank Problem - Absolute Permutation

--- coding_test/hackerrank/absolute_permutation/solution.go
+++ coding_test/hackerrank/absolute_permutation/solution.go
@@ -1,29 +1,27 @@
 package absolute_permutation
 
+import "math"
+
 // Absolute Permutation
 // https://www.hackerrank.com/challenges/absolute-permutation/problem?isFullScreen=false
 
 func absolutePermutation(n int32, k int32) []int32 {
 	p := make([]int32, n)
-	isAdd := true
 
 	if k == 0 {
 		for i := int32(1); i <= n; i++ {
 			p[i-1] = i
 		}
-	} else if n%(2*k) == 0 {
-		for i := int32(1); i <= n; i++ {
-			if isAdd {
-				p[i-1] = i + k
+	} else if n%2 != 0 || n/2 < k || math.Mod(float64(n)/float64(k), 2) != 0 {
+		p = []int32{-1}
+	} else {
+		for i := int32(0); i < n; i++ {
+			if (i/k)%2 == 0 {
+				p[i] = (i + 1) + k
 			} else {
-				p[i-1] = i - k
-			}
-			if i%k == 0 {
-				isAdd = !isAdd
+				p[i] = (i + 1) - k
 			}
 		}
-	} else {
-		p = []int32{-1}
 	}
 
 	return p
