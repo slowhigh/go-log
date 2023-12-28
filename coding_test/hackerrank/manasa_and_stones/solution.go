7da5e7765cd5fe4ba6a8cd13a8ac1a5593f02e17
Jo YoHan <whdygks210@naver.com>
2023-12-28 16:40:26 +0900

    solved: HackerRank Problem - Manasa and Stones

---
+++ coding_test/hackerrank/manasa_and_stones/solution.go
@@ -0,0 +1,24 @@
+package manasa_and_stones
+
+// Manasa and Stones
+// https://www.hackerrank.com/challenges/manasa-and-stones/problem?isFullScreen=false
+
+func stones(n int32, a int32, b int32) []int32 {
+	check := make(map[int32]bool)
+	lastStones := make([]int32, 0)
+
+	if a > b {
+		a, b = b, a
+	}
+
+	for i := int32(0); i < n; i++ {
+		lastStone := (b * i) + (a * (n - 1 - i))
+
+		if ok := check[lastStone]; !ok {
+			check[lastStone] = true
+			lastStones = append(lastStones, lastStone)
+		}
+	}
+
+	return lastStones
+}
