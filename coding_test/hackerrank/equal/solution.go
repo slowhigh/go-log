a3ddc4f2dc1b7846d05b6a41806e49b81cb9ae60
Jo YoHan <whdygks210@naver.com>
2024-02-19 19:59:08 +0900

    solving: HackerRank Problem - Equal

--- coding_test/hackerrank/equal/solution.go
+++ coding_test/hackerrank/equal/solution.go
@@ -1,76 +1,53 @@
 package equal
 
 import (
-	"fmt"
 	"math"
-	"sort"
 )
 
 // Equal
 // https://www.hackerrank.com/challenges/equal/problem?isFullScreen=false
 
-type cast struct {
-	count int32
-	piece int32
-	arr   []int32
-}
-
 func equal(arr []int32) int32 {
-	pieces := []int32{1, 2, 5}
+	var (
+		min, max int32
+		pieces   = []int32{5, 2, 1}
+		count    = int32(0)
+		pivot    = 0
+	)
 
-	min := int32(math.MaxInt32)
-	stack := make([]cast, 0)
+	for {
+		min, max = int32(math.MaxInt32), int32(0)
+		for i, v := range arr {
+			if v < min {
+				min = v
+			}
 
-	sort.Slice(arr, func(i, j int) bool { return arr[i] > arr[j] })
+			if max < v {
+				max = v
+				pivot = i
+			}
+		}
 
-	//fmt.Printf("sort %+v\n", arr)
+		for _, v := range pieces {
+			if min <= arr[pivot]-v {
+				arr[pivot] = arr[pivot] - v
+				count++
+				continue
+			}
+		}
 
-	for _, piece := range pieces {
-		if arr[0] >= piece {
-			newArr := make([]int32, len(arr))
-			copy(newArr, arr)
-			stack = append(stack, cast{1, piece, newArr})
+		equal := true
+		for i := 1; i < len(arr); i++ {
+			if arr[i-1] != arr[i] {
+				equal = false
+				break
+			}
+		}
+
+		if equal {
+			break
 		}
 	}
 
-	fmt.Printf("stack %+v\n", stack)
-
-	// for len(stack) != 0 {
-        for i := 0; i < 2; i++ {
-		c := stack[len(stack) - 1]
-		stack = stack[:len(stack) - 1]
-		//fmt.Printf("dequeue %+v\n", c)
-		fmt.Printf("stack %+v\n", stack)
-		c.arr[0] -= c.piece
-
-		isSame := true
-		for i := 0; i < len(c.arr)-1; i++ {
-			if c.arr[i] != c.arr[i+1] {
-				isSame = false
-			}
-		}
-
-		if isSame {
-			//fmt.Printf("same %+v\n", c)
-			if c.count < min {
-				min = c.count
-			}
-			continue
-		}
-
-		sort.Slice(c.arr, func(i, j int) bool { return c.arr[i] > c.arr[j] })
-		//fmt.Printf("sort %+v\n", c.arr)
-
-		for _, piece := range pieces {
-			if c.arr[0] >= piece {
-				newArr := make([]int32, len(c.arr))
-				copy(newArr, c.arr)
-				stack = append(stack, cast{c.count + 1, piece, newArr})
-			}
-		}
-
-		//fmt.Printf("queue %+v\n", queue)
-	}
-
-	return min
+	return count
 }
