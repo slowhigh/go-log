99eedb7e034af2774d26444a7751fb2258799548
Jo YoHan <whdygks210@naver.com>
2024-02-21 18:59:21 +0900

    solved: HackerRank Problem - Minimum Loss

--- coding_test/hackerrank/minimum_loss/solution.go
+++ coding_test/hackerrank/minimum_loss/solution.go
@@ -1,22 +1,35 @@
 package minimum_loss
 
-import "math"
+import (
+	"math"
+	"sort"
+)
 
 // Minimum Loss
 // https://www.hackerrank.com/challenges/minimum-loss/problem?isFullScreen=false
 
+type pack struct {
+	Price int64
+	Index int
+}
+
 func minimumLoss(price []int64) int32 {
 	var (
-		loss = int32(math.MaxInt32)
+		loss = int64(math.MaxInt64)
+		arr  = make([]pack, 0)
 	)
 
-	for i := 0; i < len(price)-1; i++ {
-		for j := i + 1; j < len(price); j++ {
-			if price[i] >= price[j] && int32(price[i]-price[j]) < loss {
-				loss = int32(price[i] - price[j])
-			}
+	for i, p := range price {
+		arr = append(arr, pack{Price: p, Index: i})
+	}
+
+	sort.Slice(arr, func(i, j int) bool { return arr[i].Price > arr[j].Price })
+
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i].Index < arr[i+1].Index && arr[i].Price-arr[i+1].Price < loss {
+			loss = arr[i].Price - arr[i+1].Price
 		}
 	}
 
-	return loss
+	return int32(loss)
 }
